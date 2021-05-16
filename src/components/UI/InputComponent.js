da51ada4c4d3efe7a0dc6a3830ae1de4800b3fed
Sharon P <sp@morganmckinley.com>
2021-05-16 19:54:43 +0530

    updated food app

--- src/components/UI/InputComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const InputComponent = (props) => {
-    return(
-        <input type={props.type ? props.type : "text"} className={props.styleClass} onChange={props.onChangeHandler} />
-    )
-}
-
-export default InputComponent;
